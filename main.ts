4093bd78dde1262ec4470918e597eaf52c3722c3
Juan Manuel Muñoz del Cerro <122436923+Jmunozc3@users.noreply.github.com>
2023-12-30 18:40:57 +0100

    main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
-import { ApolloServer } from "npm:@apollo/server@^4.9.5";
-import { startStandaloneServer } from "npm:@apollo/server@^4.9.5/standalone";
-
-import mongoose from "npm:mongoose@8.0.3";
 
 
-    const MONGO_URL = "mongodb+srv://alananconaortiz:12345@practicaoptional.8u5fio9.mongodb.net/?retryWrites=true&w=majority";
-    await mongoose.connect(MONGO_URL);
+
+   
     console.info("MongoDB connected");
 
 
     
-    console.log(`Server ready at ${url}`);
+    
